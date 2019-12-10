6b70427b1cafc19fa99788a18f1582a47002c147
tpodman172 <tpodman172@gmai.com>
2019-12-11 06:35:22 +0900

    jupiterテスト環境構築

--- server/src/main/java/com/tpodman172/uzomuzo/server/context/task/TaskRepository.java
+++ server/src/main/java/com/tpodman172/uzomuzo/server/context/task/TaskRepository.java
@@ -1,11 +1,43 @@
 package com.tpodman172.uzomuzo.server.context.task;
 
+import com.tpodman172.uzomuzo.infra.schema.rds.eden.Tables;
+import org.jooq.DSLContext;
+import org.jooq.Record;
+import org.jooq.Result;
+import org.jooq.SQLDialect;
+import org.jooq.impl.DSL;
+import org.springframework.stereotype.Component;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
 import java.util.List;
 
-class TaskRepository implements ITaskRepository {
+
+@Component
+public class TaskRepository implements ITaskRepository {
 
     @Override
     public List<TaskEntity> find() {
+        final String userName = "root";
+        final String password = "root";
+        final String url = "jdbc:mysql://localhost:3306/tsk2";
+
+        try (Connection conn = DriverManager
+                .getConnection(url, userName, password);
+             final DSLContext create = DSL.using(conn, SQLDialect.MYSQL)) {
+            Result<Record> result = create.select().from(Tables.TASKS).fetch();
+
+            for (final Record r : result) {
+                final Integer id = r.getValue(Tables.TASKS.ID);
+                final String title = r.getValue(Tables.TASKS.TITLE);
+
+                System.out.println(
+                        "ID: " + id + " title: " + title);
+            }
+        } catch (final Exception e) {
+            e.printStackTrace();
+        }
+
         return null;
     }
 }
---
+++ server/src/test/java/com/tpodman172/uzomuzo/server/context/task/TaskRepositoryTest.java
@@ -0,0 +1,18 @@
+package com.tpodman172.uzomuzo.server.context.task;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class TaskRepositoryTest {
+
+    //    @Autowired
+//    private ITaskRepository taskRepository;
+    @Test
+    @DisplayName("2 * 3 = 6 であること😆")
+    void test() throws Exception {
+        assertThat(2 * 3).isEqualTo(6);
+    }
+
+}
